67d195833725fa42806d4fc1d2bfb0f7b89d508d
deon-gracias <deongracias1@gmail.com>
2022-06-17 20:49:01 +0530

    Clean up

--- app.js
+++ app.js
@@ -7,53 +7,42 @@ const mongoose = require("mongoose");
 const userRouter = require("./routes/userRouter");
 const dotenv = require("dotenv");
 const cors = require("cors");
-
+const watchlistRouter = require("./routes/watchlistRouter");
 
 dotenv.config();
+
 const port = process.env.PORT || 5001;
+
 mongoose
-  .connect("mongodb+srv://root:root@cluster0.qo4qm.mongodb.net/Crypton?retryWrites=true&w=majority", { autoCreate: true })
+  .connect(
+    "mongodb+srv://root:root@cluster0.qo4qm.mongodb.net/Crypton?retryWrites=true&w=majority",
+    { autoCreate: true }
+  )
   .then(() => console.log("Connected to MongoDB"))
   .catch((err) => console.log("Couldn't connect to Atlas: ", err.message));
 
 // Middleware
 const app = express();
-app.use(express.json());
-app.use(cors());
-app.use(express.urlencoded({ extended: true }));
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
 
 // Logger
 app.use(logger("dev"));
 
-// View engine setup
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "jade");
-
-// Static Pages
-app.use(express.static(path.join(__dirname, "public")));
-
 // Routes
+app.get("/", (req, res) => res.send("Hell World"))
 app.use("/users", userRouter);
+app.use("/watchlist", watchlistRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
   next(createError(404));
 });
 
-// Error handler
-app.use(function (err, req, res, next) {
-  // Set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get("env") === "Development" ? err : {};
-
-  // Render the error page
-  res.status(err.status || 500);
-  res.render("Error");
-});
-
 app.listen(port, () => {
-  console.log('Server started on: ' + port);
+  console.log("Server started on: " + port);
 });
 
-module.exports = app;
+// module.exports = app;
